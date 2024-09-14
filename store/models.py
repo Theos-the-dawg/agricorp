71bea5f02be9c074eccfae5e6691dda5e3913021
Motheo <75973965+Theos-the-dawg@users.noreply.github.com>
2024-09-14 21:04:07 +0200

    invoice-model/datatable-setup

--- store/models.py
+++ store/models.py
@@ -1,6 +1,35 @@
 from django.db import models
 from datetime import datetime
 
+class Invoice(models.Model):
+    Accounting =models.DecimalField(max_digits=10, decimal_places=2)
+    Advertising =models.DecimalField(max_digits=10, decimal_places=2)
+    Bank_charges = models.DecimalField(max_digits=10, decimal_places=2)
+    Cleaning = models.DecimalField(max_digits=10, decimal_places=2)
+    Borehole_Implements = models.DecimalField(max_digits=10, decimal_places=2)
+    Tractor_hiring = models.DecimalField(max_digits=10, decimal_places=2)
+    Tomato_Trellising_twine = models.DecimalField(max_digits=10, decimal_place)
+    Consulting_Fees = models.DecimalField(max_digits=10, decimal_place)
+    Council_Unit_Tax = models.DecimalField(max_digits=10, decimal_place)
+    Medicines_Vet = models.DecimalField(max_digits=10, decimal_place)
+    Fuels_Oils = models.DecimalField(max_digits=10, decimal_place)
+    Spraying_chemicals = models.DecimalField(max_digits=10, decimal_place)
+    General_expenses = models.DecimalField(max_digits=10, decimal_place)
+    Insurance_licenses = models.DecimalField(max_digits=10, decimal_place)
+    Stockfeeds = models.DecimalField(max_digits=10, decimal_place)
+    M/V_Maintanance = models.DecimalField(max_digits=10, decimal_place)
+    Tractor_Repairs = models.DecimalField(max_digits=10, decimal_place)
+    Seeds_tomatoes = models.DecimalField(max_digits=10, decimal_place)
+    seeds_beans = models.DecimalField(max_digits=10, decimal_place)
+    Salaries_wages = models.DecimalField(max_digits=10, decimal_place)
+    Security = models.DecimalField(max_digits=10, decimal_place)
+    Fertiliser = models.DecimalField(max_digits=10, decimal_place)
+    Telephones_Fax_Internet = models.DecimalField(max_digits=10, decimal_place)
+    Training = models.DecimalField(max_digits=10, decimal_place)
+    Travelling = models.DecimalField(max_digits=10, decimal_place)
+    Staff_wefare = models.DecimalField(max_digits=10, decimal_place)
+    Loan_Repayment = models.DecimalField(max_digits=10, decimal_place)
+
 class Harvest(models.Model):
     crop = models.CharField(max_length=50)
     crops = models.JSONField()
