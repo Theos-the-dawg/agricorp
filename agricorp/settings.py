cacc033fe2ec41459555aaeeec9f5fbfbc03f74a
Theos-the-dawg <moteo13@outlook.com>
2024-09-17 21:54:19 +0200

    fix-models-and-setup-venv-properly

--- agricorp/settings.py
+++ agricorp/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
-    'shell_plus',
-    'django_extensions',
+    'store'
 ]
 
 MIDDLEWARE = [
