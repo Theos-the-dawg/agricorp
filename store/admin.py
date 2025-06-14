ae44a2a0a3f86a094791c411e83df15f9b241f09
theos-the-dawg <motheo13@outlook.com>
2025-06-14 15:47:21 +0200

    save

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import ExpenseCategory,ExpenseEntry,Expense
+from .models import *
 # Register your models here.
 admin.site.register(ExpenseEntry)
 admin.site.register(ExpenseCategory)
 admin.site.register(Expense)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
