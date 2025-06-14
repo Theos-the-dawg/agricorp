111eaaa2c70d4595927c4a437eed93acd5185c10
theos-the-dawg <motheo13@outlook.com>
2025-06-14 21:34:03 +0200

    changes-for-up-loading-to-vercel

--- agricorp/settings.py
+++ agricorp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ktp641dhqro#=o--rfi8w%&!=erwgmz=n7viojgi819f#rapm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
