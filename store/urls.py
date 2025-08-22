a685b0245c3a4a73df0e06315fe1dc273ba34a56
Theos-the-dawg <motheo13@outlook.com>
2025-08-22 19:26:40 +0200

    make-changes-to-home-page-and-allow-products-to-be-viewed

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from .views import (home_view,login_view,confirm_logout_view,
                      register,add_expenses,generate_dataframe,
-                     product_list,product_detail,place_order)
+                     category_list,product_list,product_detail,place_order)
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,7 +14,8 @@ urlpatterns = [
   path('register/', register, name='register'),
   path('add_expenses/',add_expenses, name='add_expenses'),
   path('dataframe/', generate_dataframe, name='generate_dataframe'),
-  path('category/<int:category_id>/', product_list, name='product_list'),
+  path('categories/', category_list, name='category_list'),
+  path('products/<int:category_id>/', product_list, name='product_list'),
   path('product/<int:product_id>/', product_detail, name='product_detail'),
   path('order/<int:product_id>/', place_order, name='place_order'),
 
